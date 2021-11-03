e9da1379dc236bcdffada8a0185d0634cb0ed9a5
Rus <de.mr44@gmail.com>
2021-11-03 15:59:59 +0500

    Set up a queue with a work item

--- Multithreading-GCD4/ViewController.swift
+++ Multithreading-GCD4/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    private let imageURL: URL = URL(string: "https://images.unsplash.com/photo-1635813782590-0f5d34934b9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 15
     }
-
-
+    
+    private func setUpQueue() {
+        var data: Data?
+        let queue = DispatchQueue.global(qos: .utility)
+        let workItem = DispatchWorkItem(qos: .userInteractive) {
+            data = try? Data(contentsOf: self.imageURL)
+        }
+        
+        queue.async(execute: workItem)
+        
+        workItem.notify(queue: DispatchQueue.main) {
+            if let imageData = data {
+                self.imageView.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        imageView.image = nil
+        setUpQueue()
+    }
 }
 
